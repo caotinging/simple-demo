c0cf67e75d61369f185c5588cfacaafa88deff1d
yizhanhong <745607721@qq.com>
2019-06-12 18:07:31 +0800

    新增关于NIO的demo

---
+++ nio-demo/src/main/java/com/bise/niodemo/channel/FileChannelDemo.java
@@ -0,0 +1,36 @@
+package com.bise.niodemo.channel;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @Author: bise
+ * @Date: 2019/6/12 18:01
+ * @Version 1.0
+ */
+public class FileChannelDemo {
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile aFile = new RandomAccessFile("D:/NioDemoFiles/nio-data.txt", "rw");
+        FileChannel inChannel = aFile.getChannel();
+
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead != -1) {
+
+            System.out.println("Read " + bytesRead);
+            buf.flip();
+
+            while(buf.hasRemaining()){
+                System.out.print((char) buf.get());
+            }
+
+            buf.clear();
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+    }
+}
