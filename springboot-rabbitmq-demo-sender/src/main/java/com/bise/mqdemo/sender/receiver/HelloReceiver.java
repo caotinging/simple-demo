b73dec964181000af36ec158c6d9df47f810c634
caotinging <caotinging@outlook.com>
2020-04-07 19:04:17 +0800

    【springboot+RabbitMQ】更新model结构，mq test目录下可进行简单测试

--- springboot-rabbitmq-demo-sender/src/main/java/com/bise/mqdemo/sender/receiver/HelloReceiver.java
+++ springboot-rabbitmq-demo-sender/src/main/java/com/bise/mqdemo/sender/receiver/HelloReceiver.java
@@ -5,17 +5,32 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 /**
+ * @desc: <p>
+ *     经测试sender发送消息的时候这里三个handler是均匀的接收的，（但不是每条message都能让每个handler接收到
+ * </p>
  * @program: simple-demo
  * @description: RabbitMQ入门-消息接收者
  * @author: CaoTing
  * @date: 2020/4/7
  */
 @Component
-@RabbitListener(queues = {"helloMQ"})
 public class HelloReceiver {
 
     @RabbitHandler
-    public void process(String hello) {
-        System.out.println("Receiver  : " + hello);
+    @RabbitListener(queues = {"helloMQ"})
+    public void process1(String hello) {
+        System.out.println("Receiver1  : " + hello);
+    }
+
+    @RabbitHandler
+    @RabbitListener(queues = {"helloMQ"})
+    public void process2(String hello) {
+        System.out.println("Receiver2  : " + hello);
+    }
+
+    @RabbitHandler
+    @RabbitListener(queues = {"helloMQ"})
+    public void process3(String hello) {
+        System.out.println("Receiver3  : " + hello);
     }
 }
